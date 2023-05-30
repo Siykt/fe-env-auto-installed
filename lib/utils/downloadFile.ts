1a25758410f8d83b198098fafba2c339a8866678
siykt <cnsiykt@163.com>
2023-05-30 13:20:49 +0800

    fix: downloadFile type

--- lib/utils/downloadFile.ts
+++ lib/utils/downloadFile.ts
@@ -3,7 +3,7 @@ import { IPCEvent } from '@/modules/IPCEvent/Core';
 import type { Progress } from 'electron-dl';
 
 export default function downloadFile(url: string, onProgress?: (progress: Progress) => void) {
-  return new Promise((resolve, reject) => {
+  return new Promise<string>((resolve, reject) => {
     ipcRenderer.send(IPCEvent.DownloadFile, url);
     ipcRenderer.on(IPCEvent.DownloadFileComplete, (_event, data) => resolve(data));
     ipcRenderer.on(IPCEvent.DownloadFileError, (_event, error) => reject(error));
